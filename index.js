9336d6d803e72047a648558f937be779b5c5da64
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-05-08 14:38:57 -0500

    Added files

---
+++ index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [`package name: `,`version: `,`description: `,`entry point: `,`test command: `,`git repository: `,`keywords: `,`author: `,`license: `];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
